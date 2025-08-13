3f6b438e7ce3216c406197752fdc68b2f1894caf
adriansito <adriancrosby02@gmail.com>
2025-08-12 22:07:48 -0700

    fixed broken button on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -400,15 +400,11 @@ export default function Landing() {
 				</ul>
 			</section>
 			<section className="w-full mx-auto text-center my-8">
-				<button
-					onClick={() => {
-						navigate('/builder');
-						window.scrollTo({ top: 0, behavior: 'smooth' });
-					}}
-					className="rounded-xl px-8 py-4 bg-gradient-to-r from-pink-500 to-indigo-500 text-white text-lg font-bold hover:from-pink-600 hover:to-indigo-600 transition-all duration-300 shadow-lg transform hover:scale-105"
-				>
-					Build My Resume
-				</button>
+				<Link to="builder">
+					<button className="rounded-xl px-8 py-4 bg-gradient-to-r from-pink-500 to-indigo-500 text-white text-lg font-bold hover:from-pink-600 hover:to-indigo-600 transition-all duration-300 shadow-lg transform hover:scale-105">
+						Build My Resume
+					</button>
+				</Link>
 			</section>
 		</main>
 	);
